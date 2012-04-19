a5343b8ac997492aa33e45d719819d6b4b0a1f99
Matt Hoffman <hoffmanm@cs.ubc.ca>
2012-04-18 21:54:48 -0700

    Got rid of code in the demo too.

--- demo.py
+++ demo.py
@@ -18,11 +18,3 @@ model = mogmdp.MoGMDP(**params)
 policy = mogmdp.MoGPolicy(-0.8, 0.0, 1.0)
 trans = mogmdp.ZTransition(model, policy)
 
-forward = [mogmdp.ForwardMessage.init(model, policy)]
-for k in xrange(20):
-    forward.append(forward[-1].get_next(trans))
-
-beta = mogmdp.BackwardMessage.init(model)
-
-# print mogmdp.get_jtheta(model, policy, 0.95, 100)
-# print mogmdp.get_moments(model, policy, 0.95, 100)
