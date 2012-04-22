Fixed bug in the computation of CC.

demo.py
@@ -14,6 +14,11 @@ params['M'] = 3*np.eye(2)
 params['L'] = 2*np.eye(2)
 params['w'] = 1.0
 
-model = mogmdp.MoGMDP(**params)
-policy = mogmdp.MoGPolicy(-0.8, 0.0, 1.0)
+H = 20
+theta = [-0.8, 0.2, 1.0]
+gamma = 0.95
 
+model = mogmdp.MoGMDP(**params)
+gradient = mogmdp.get_gradient(model, model.unpack_policy(theta), gamma, H)
+
+print gradient

mogmdp.py
@@ -183,7 +183,7 @@ def get_gradient(model, policy, gamma, H):
     # the action minus the "predicted-action".
     M = np.c_[-policy.K, np.eye(model.na)]
     A = np.r_[np.c_[np.inner(policy.K, policy.K), -policy.K.T], M]
-    CC = np.trace(np.dot(A, ZZ)) + J*np.inner(policy.m, policy.m) - 2*np.inner(policy.m, np.dot(M, Z))
+    CC = np.trace(np.dot(A, ZZ)) + Js*np.inner(policy.m, policy.m) - 2*np.inner(policy.m, np.dot(M, Z))
 
     dK = (policy.sigma**-2) * UX - np.dot(policy.K, XX) - np.outer(policy.m, X)
     dm = (policy.sigma**-2) * U - np.dot(policy.K, X) - Js*policy.m