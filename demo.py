37b14060deb39ec1cc1838da7614bcb1af1d7dfa
Matt Hoffman <hoffmanm@cs.ubc.ca>
2012-05-01 19:50:59 -0700

    Updated the demo to look better.

--- demo.py
+++ demo.py
@@ -1,10 +1,9 @@
 from __future__ import division
-
 import numpy as np
 import matplotlib.pyplot as pl
 import mogmdp
-import lbfgsb
 
+# construct the model.
 params = {}
 params['mu0'] = -5.0
 params['Sigma0'] = 0.5
@@ -16,13 +15,15 @@ params['y'] = [[3.,0.], [2.,0.]]
 params['M'] = [3*np.eye(2), np.eye(2)]
 params['L'] = [2*np.eye(2), np.eye(2)]
 
+# construct parameters under which we'll optimize the model.
 H = 50
 gamma = 0.95
 sigma_min = 0.001
-theta0 = [-.8, -0.0, 1.0]
+theta = [-.8, -0.0, 1.0]
 
+# create the actual model/policy objects.
 model = mogmdp.MoGMDP(**params)
-policy0 = model.unpack_policy(theta0)
+policy = model.unpack_policy(theta)
 
 # construct the contours.
 X, Y = np.meshgrid(np.linspace(-2,0,20), np.linspace(-.5,3,20))
@@ -30,30 +31,26 @@ J = np.empty(X.shape)
 for i, (K, m) in enumerate(zip(X.flat, Y.flat)):
     J.flat[i] = mogmdp.get_jtheta(model, model.unpack_policy([K, m, sigma_min]), gamma, H)
 
-_, _, info_gem = mogmdp.solve_mogmdp(model, policy0, gamma, H, sigma_min)
-_, _, info_pem = mogmdp.solve_mogmdp(model, policy0, gamma, H, sigma_min, em=True)
-_, _, info_em  = mogmdp.solve_mogmdp_em(model, policy0, gamma, H, maxfun=50)
+# solve the problem using a few different methods.
+_, _, info_gem = mogmdp.solve_mogmdp(model, policy, gamma, H, sigma_min)
+_, _, info_pem = mogmdp.solve_mogmdp(model, policy, gamma, H, sigma_min, em=True)
+_, _, info_em  = mogmdp.solve_mogmdp_em(model, policy, gamma, H, maxfun=20)
 
+titles = ['LBFGS', 'LBFGS-EM', 'vanilla EM']
+infos = [info_gem, info_pem, info_em]
+
+# plot the paths taken.
+pl.figure(figsize=(12,4))
+for i, (title, info) in enumerate(zip(titles, infos)):
+    pl.subplot(1,3,i)
+    pl.contour(X, Y, J)
+    pl.plot(info['theta'][:,0], info['theta'][:,1], 'r-', lw=2)
+    pl.title(title)
+
+# plot the performance results.
 pl.figure()
-pl.subplot(2,2,1)
-pl.contour(X, Y, J)
-pl.plot(info_gem['theta'][:,0], info_gem['theta'][:,1], 'r-', lw=2)
-pl.title('LBFGS')
-
-pl.subplot(2,2,2)
-pl.contour(X, Y, J)
-pl.plot(info_pem['theta'][:,0], info_pem['theta'][:,1], 'r-', lw=2)
-pl.title('LBFGS-EM')
-
-pl.subplot(2,2,3)
-pl.contour(X, Y, J)
-pl.plot(info_em['theta'][:,0], info_em['theta'][:,1], 'r-', lw=2)
-pl.title('EM')
-
-pl.figure()
-pl.plot(info_gem['numevals'], info_gem['jtheta'], lw=2, label='LBFGS')
-pl.plot(info_pem['numevals'], info_pem['jtheta'], lw=2, label='LBFGS-EM')
-pl.plot(info_em ['numevals'], info_em ['jtheta'], lw=2, label='vanilla EM')
+for title, info in zip(titles, infos):
+    pl.plot(info['numevals'], info['jtheta'], lw=2, label=title)
 pl.legend(loc='lower right')
 pl.xlabel('function evaluations')
 pl.ylabel('expected reward')
